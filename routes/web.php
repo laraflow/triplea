84dccff7a56dfe945789a64fb2ec6b2be1754598
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-07-13 04:06:01 +0600

    namespac middleware fixed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
 use Laraflow\TripleA\Http\Controllers\Auth\AuthenticatedSessionController;
 use Laraflow\TripleA\Http\Controllers\Auth\ConfirmablePasswordController;
 use Laraflow\TripleA\Http\Controllers\Auth\EmailVerificationNotificationController;
@@ -7,67 +9,69 @@ use Laraflow\TripleA\Http\Controllers\Auth\EmailVerificationPromptController;
 use Laraflow\TripleA\Http\Controllers\Auth\PasswordResetController;
 use Laraflow\TripleA\Http\Controllers\Auth\RegisteredUserController;
 use Laraflow\TripleA\Http\Controllers\Auth\VerifyEmailController;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Route;
+
 /**
  * Authentication Route
  */
-Route::name('triplea.auth.')->prefix(Config::get('triplea.auth.prefix'))->group(function () {
+Route::name('triplea.auth.')
+    ->middleware('web')
+    ->prefix(Config::get('triplea.auth.prefix'))
+    ->group(function () {
 
-    Route::get('/login', AuthenticatedSessionController::class)
-        ->middleware('guest')
-        ->name('login');
-
-    Route::post('/login', [AuthenticatedSessionController::class, 'login'])
-        ->middleware('guest');
-
-    Route::match(['get', 'post'], '/logout', [AuthenticatedSessionController::class, 'logout'])
-        ->middleware('auth')
-        ->name('logout');
-
-    if (Config::get('triplea.allow_register')):
-        Route::get('/register', [RegisteredUserController::class, 'create'])
+        Route::get('/login', AuthenticatedSessionController::class)
             ->middleware('guest')
-            ->name('register');
+            ->name('login');
 
-        Route::post('/register', [RegisteredUserController::class, 'store'])
+        Route::post('/login', [AuthenticatedSessionController::class, 'login'])
             ->middleware('guest');
-    endif;
 
-    if (Config::get('triplea.allow_password_reset')):
-        Route::get('/forgot-password', [PasswordResetController::class, 'create'])
+        Route::match(['get', 'post'], '/logout', [AuthenticatedSessionController::class, 'logout'])
+            ->middleware('auth')
+            ->name('logout');
+
+        if (Config::get('triplea.allow_register')):
+            Route::get('/register', [RegisteredUserController::class, 'create'])
+                ->middleware('guest')
+                ->name('register');
+
+            Route::post('/register', [RegisteredUserController::class, 'store'])
+                ->middleware('guest');
+        endif;
+
+        if (Config::get('triplea.allow_password_reset')):
+            Route::get('/forgot-password', [PasswordResetController::class, 'create'])
+                ->middleware('guest')
+                ->name('password.request');
+        endif;
+
+        Route::post('/forgot-password', [PasswordResetController::class, 'store'])
             ->middleware('guest')
-            ->name('password.request');
-    endif;
+            ->name('password.email');
 
-    Route::post('/forgot-password', [PasswordResetController::class, 'store'])
-        ->middleware('guest')
-        ->name('password.email');
+        Route::get('/reset-password/{token}', [PasswordResetController::class, 'edit'])
+            ->middleware('guest')
+            ->name('password.reset');
 
-    Route::get('/reset-password/{token}', [PasswordResetController::class, 'edit'])
-        ->middleware('guest')
-        ->name('password.reset');
+        Route::post('/reset-password', [PasswordResetController::class, 'update'])
+            ->middleware('guest')
+            ->name('password.update');
 
-    Route::post('/reset-password', [PasswordResetController::class, 'update'])
-        ->middleware('guest')
-        ->name('password.update');
+        Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+            ->middleware('auth')
+            ->name('verification.notice');
 
-    Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
-        ->middleware('auth')
-        ->name('verification.notice');
+        Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+            ->middleware(['auth', 'signed', 'throttle:6,1'])
+            ->name('verification.verify');
 
-    Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-        ->middleware(['auth', 'signed', 'throttle:6,1'])
-        ->name('verification.verify');
+        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+            ->middleware(['auth', 'throttle:6,1'])
+            ->name('verification.send');
 
-    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware(['auth', 'throttle:6,1'])
-        ->name('verification.send');
+        Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+            ->middleware('auth')
+            ->name('password.confirm');
 
-    Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->middleware('auth')
-        ->name('password.confirm');
-
-    Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
-        ->middleware('auth');
-});
+        Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+            ->middleware('auth');
+    });
